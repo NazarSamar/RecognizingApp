7453ca261f38e4c6c30781ec9e8d60264ef1c10c
Nazar Samardak <45075284+NazarSamar@users.noreply.github.com>
2022-12-13 18:05:49 +0200

    added helper classes

---
+++ PictureRecognition/Helpers/FileUploadSample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+using Swashbuckle.Swagger;
+using Microsoft.OpenApi.Models;
+
+namespace PictureRecognition.Helpers
+{
+    
+
+    namespace FileUploadSample
+    {
+        public class SwaggerFileOperationFilter : Swashbuckle.AspNetCore.SwaggerGen.IOperationFilter
+        {
+            public void Apply(OpenApiOperation operation, OperationFilterContext context)
+            {
+
+                operation.Parameters.Add(
+                    new OpenApiParameter
+                    {
+                        Name = "Files",
+                        In = ParameterLocation.Header,
+                        Description = "Upload File",
+                        Required = true,
+                        Schema = new OpenApiSchema
+                        {
+                            Type = "file",
+                            Format = "binary"
+                        }
+                    }
+                );
+               
+            }
+        }
+    }
+}
---
+++ PictureRecognition/Helpers/ScriptRunner.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureRecognition.Helper
+{
+    public static class ScriptRunner
+    {
+        //args separated by spaces
+        public static string RunFromCmd(string rCodeFilePath, string args)
+        {
+            string file = rCodeFilePath;
+            string result = string.Empty;
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            try
+            {
+
+                var info = new ProcessStartInfo(@"E:\Anaconda\envs\nenv\python.exe");
+                info.Arguments = rCodeFilePath + " " + args;
+
+                info.RedirectStandardInput = false;
+                info.RedirectStandardOutput = true;
+                info.UseShellExecute = false;
+                info.CreateNoWindow = true;
+                info.StandardOutputEncoding = Encoding.GetEncoding(21866);
+
+                using (var proc = new Process())
+                {
+                    proc.StartInfo = info;
+                    proc.Start();
+                    proc.WaitForExit();
+                   
+                    result = proc.StandardOutput.ReadToEnd();
+                    
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("R Script failed: " + result, ex);
+            }
+        }
+    }
+}
