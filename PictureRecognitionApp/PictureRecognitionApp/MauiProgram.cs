f6013a5ee3c3f3ca52ea56f085a61f8af1df58f1
Nazar Samardak <45075284+NazarSamar@users.noreply.github.com>
2022-12-13 18:10:30 +0200

    upload

---
+++ PictureRecognitionApp/PictureRecognitionApp/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Maui.LifecycleEvents;
+using PictureRecognitionApp.Data;
+
+namespace PictureRecognitionApp;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("e-Ukraine-Bold.otf", "e-Ukraine-Bold");
+                fonts.AddFont("e-Ukraine-Medium.otf", "e-Ukraine-Medium");
+                fonts.AddFont("e-Ukraine-Regular.otf", "e-Ukraine-Regular");                                
+            });
+
+		builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+		builder.Services.AddSingleton<ImageService>();
+
+        return builder.Build();
+	}
+}
