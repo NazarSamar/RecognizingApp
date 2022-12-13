faa44f424e3ea1c394974e7908f17370636a6b2b
Nazar Samardak <45075284+NazarSamar@users.noreply.github.com>
2022-12-13 18:14:53 +0200

    uploaded additional files

---
+++ PictureRecognitionApp/PictureRecognitionApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PictureRecognitionApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
